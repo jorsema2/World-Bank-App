8837d5603d1519c1207f8eef6cdbcfd3fc9e67b2
Jorge <jorsema2@gmail.com>
2020-08-08 20:41:36 +0200

    Dropdown installed, but it doesn't work properly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Searcher from "./components/Searcher"
 import CountriesList from "./components/CountriesList"
+import Dropdown from '../node_modules/react-dropdown';
+import '../node_modules/react-dropdown/style.css';
 
 const App = () => {
     const [allCountries, setAllCountries] = useState([]);
@@ -10,6 +12,7 @@ const App = () => {
         <div>
             <h1>World Bank App</h1>
             <Searcher setAllCountries={setAllCountries} allCountries={allCountries} chosenCountries={chosenCountries} setChosenCountries={setChosenCountries} />
+            <Dropdown options={allCountries} onChange={Dropdown._onSelect} value={allCountries[0]} placeholder={allCountries[0]} />
             <CountriesList allCountries={allCountries} setCountries={setAllCountries} chosenCountries={chosenCountries} setChosenCountries={setChosenCountries} />
         </div>
     )
