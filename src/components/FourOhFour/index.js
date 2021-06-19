bd523450ac2285b3543844566fdef5741b7e4327
Jorge Segura <jsegura@eventscase.com>
2021-06-19 18:37:51 +0200

    first push

--- src/components/FourOhFour/index.js
+++ src/components/FourOhFour/index.js
@@ -10,7 +10,7 @@ const FourOhFour = () => {
         The page that you want to see does not exist. This may be because an
         invalid URL was introduced.
       </p>
-      <p>Please, click in the Earth icon to go back to home.</p>
+      <p>Please, click on the Earth icon to go back to home.</p>
       <Link to="/">
         <Earth />
       </Link>
